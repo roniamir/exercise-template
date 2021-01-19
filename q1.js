e6322aac1d93cd7ef8f3035b39cb424b9d308c55
roniamir <77384841+roniamir@users.noreply.github.com>
2021-01-19 20:11:15 +0200

    q1 solution
    with JSON.parse method

--- q1.js
+++ q1.js
@@ -1,11 +1,15 @@
+
 function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
+    const theInput = JSON.parse(input);
     let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
 
-/**/
+    if(theInput.constructor === Array){
+        out = "is array";
+    } else{
+        out = "is not array";
+}
+
     output.innerText = out;
 }
